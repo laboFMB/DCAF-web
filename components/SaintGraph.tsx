a1c2e6d2d9bd5bd583269314c33d8fb1f234ffbb
laperlej <jonathan.laperle@usherbrooke.ca>
2022-09-01 15:10:17 -0400

    graph

--- components/SaintGraph.tsx
+++ components/SaintGraph.tsx
@@ -2,12 +2,12 @@ import { Scatter, ScatterChart, XAxis, YAxis, ZAxis, Tooltip } from 'recharts'
 
 export const SaintGraph = ({ data }) => {
   const rows = data.slice(1).map((rowData: string[]) => {
-    return { x: rowData[3], y: rowData[4], z: 1 }
+    return { x: parseFloat(rowData[4]), y: parseFloat(rowData[3]), z: 1 }
   })
   return (
-    <ScatterChart width={400} height={400}>
-      <YAxis dataKey="x" type="number" name="saint score" />
-      <XAxis dataKey="y" name="log2FC" />
+    <ScatterChart width={600} height={400}>
+      <XAxis dataKey="x" type="number" name="log2FC" />
+      <YAxis dataKey="y" domain={[0, 1]} type="number" name="saint score" />
       <ZAxis dataKey="z" range={[0, 30]} />
       <Scatter name="test" data={rows} fill="#8884d8" />
       <Tooltip cursor={{ strokeDasharray: '3 3' }} />
